9b9c4ed84e1e28d961c9ae53e8a50f367139c764
Josh <aftershock840@gmail.com>
2024-12-26 09:33:19 -0500

    first commit

--- js/index.js
+++ js/index.js
@@ -1,3 +1,27 @@
 // Prevent us from attempting to use variables
 // that are not declared
 "use strict"
+
+//Questions below
+
+// knowing that typeof arrays return object, how do we tell the difference between objects and arrays when working on someone else's code? is there another way to return something more specific?
+
+
+
+
+
+// let myArr = [45, "5", true];
+// // console.log(myArr);
+// // console.log(typeof myArr);
+// myArr.shift();
+// myArr.shift();
+// myArr.shift();
+// myArr.unshift("all ", "I ", "want ", "for ", "Christmas ", "is ", "my", "2 ", "front ", "teeth!");
+// // console.log(myArr);
+// // console.log(myArr.slice(1,3));
+// // console.log(myArr.slice(6,7));
+// // console.log(myArr.slice(7,10));
+
+// // let newArr = [myArr[1], myArr.slice(1,3), myArr.slice(6,7), myArr.slice(7,10)];
+
+// console.log(newArr);
