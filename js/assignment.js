232951e2a282cbba3eb70b72d95d68b88da5dc38
Josh <aftershock840@gmail.com>
2024-12-26 15:34:13 -0500

    added functions to file

--- js/assignment.js
+++ js/assignment.js
@@ -8,8 +8,9 @@ const todos = [
     "Rake the leaves",
 ];
 
-// Declare your two immutable variables
-
+// Declare your two mutable variables
+let shiftValue;
+let popValue;
 
 // Declaring constant variables to store references to the elements
 // that will be updated by your code.
@@ -22,43 +23,55 @@ const answerSixEl = document.getElementById("answer-six");
 const submissionBtn = document.getElementById("submission-btn");
 
 function updateAnswerOne() {
-    
+    answerOneEl.innerText = todos;
+    // console.log("1");
 }
 
 function updateAnswerTwo() {
-
+    answerTwoEl.innerText = todos[2];
+    console.log("2");
 }
 
 function removeLastValue() {
+    popValue = todos.pop();
 
 }
 
 function removeFirstValue() {
-
+    shiftValue = todos.shift();
 }
 
 function addShiftAndPopValues() {
-
+    todos.push(popValue);
+    todos.push(shiftValue);
 }
 
 function updateAnswerFour() {
-
+    answerFourEl.innerText = todos;
 }
 
 function reverseTodoList() {
-
+    const arrReverse = todos.reverse();
 }
 
 function updateAnswerFive() {
-
+    answerFiveEl.innerText = todos;
 }
 
 function updateAnswerSix() {
-
+    removeFirstValue();
+    addShiftAndPopValues();
+    answerSixEl.innerText = todos;
 }
 
 function render() {
-
+    updateAnswerOne();
+    updateAnswerTwo();
+    removeLastValue();
+    updateAnswerFour();
+    reverseTodoList();
+    updateAnswerFive();
+    updateAnswerSix();
 }
 
 submissionBtn.addEventListener("click", function () {
